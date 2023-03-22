eea4e6faf28b3b5fae2f093cf89ab500301758f9
Natan Melo <natan.tere2012@gmail.com>
2023-03-21 23:49:08 -0300

    CRUD

--- src/controller/crud.js
+++ src/controller/crud.js
@@ -1,51 +1,97 @@
-const DB = require('../data/connection')
 const veiculos = require('../model/veiculos')
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const router = require('express').Router()
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 module.exports = router;
 let veiculo = []
 const control = {
     read() {
-        veiculo = JSON.parse(fs.readFileSync('../data/veiculos.json', { encoding: 'utf8' }))
+        veiculo = JSON.parse(fs.readFileSync('src/data/veiculos.json', { encoding: 'utf8' }))
         return veiculo
     },
-    create({novoVeiculo}) {
-        const data = { novoVeiculo }
+    write(novoVeiculo) {
+        const data = novoVeiculo
+        console.log(data)
         veiculo.push(data)
-        fs.writeFileSync('../data/veiculos.json', JSON.stringify(veiculos), { encoding: 'utf8' })
+        fs.writeFileSync('src/data/veiculos.json', JSON.stringify(veiculo), { encoding: 'utf8' })
+    },
+    delete(deleteVeiculo) {
+        const data = deleteVeiculo
+        veiculo.pop(data)
+        fs.writeFileSync("src/data/veiculos.json", JSON.stringify(veiculo), { encoding: 'utf8' })
     }
 }
 
-router.get('/', (req, res) => {
+router.get('/veiculo', (req, res) => {
     console.log(control.read())
+    return control.read()
 })
-router.get('/:id', (req, res) => {
+router.get('/veiculo/:id', jsonParser, (req, res) => {
     const dados = control.read()
-    const id = req.params.id
-    for (i = 0; i <= dados.length; i++){
-        if (dados[i].id == id) {
-            return dados[i]
+    const paramId = req.params.id
+    dados.forEach(veiculo => {
+        if (veiculo.id == paramId) {
+            console.log(veiculo)
+            return veiculo
         } else {
-            return msg('Veículo não encontrado')
-        }
-    }
+            return console.log('Veículo não encontrado')
+        }    
+    })
 })
-router.post('/', (req, res) => {
+router.post('/create', jsonParser, (req, res) => {
     const dados = control.read()
     const qtdDados = dados.length
-    const novoVeiculo = new veiculos()
-    novoVeiculo.id = dados[qtdDados].id + 1
-    novoVeiculo.placa = req.body.placa
-    novoVeiculo.chassi = req.body.chassi
-    novoVeiculo.renavam = req.body.renavam
-    novoVeiculo.modelo = req.body.modelo
-    novoVeiculo.marca = req.body.marca
-    novoVeiculo.ano = req.body.ano
-    control.create(novoVeiculo)
+    console.log(req.body.placa)
+    if (req.body == undefined) {
+        return console.log("Erro ao cadastrar veículo")
+    } else {
+        const novoVeiculo = new veiculos()
+        novoVeiculo.id = qtdDados + 1
+        novoVeiculo.placa = req.body.placa
+        novoVeiculo.chassi = req.body.chassi
+        novoVeiculo.renavam = req.body.renavam
+        novoVeiculo.modelo = req.body.modelo
+        novoVeiculo.marca = req.body.marca
+        novoVeiculo.ano = req.body.ano
+        control.write(novoVeiculo)
+        return console.log("Veículo cadastrado")    
+    }
 })
-router.put('/:id', (req, res) => {
-    
+router.put('/update/:id', jsonParser, (req, res) => {
+    const dados = control.read()
+    const id = req.params.id
+    dados.forEach(veiculo => {
+        console.log(veiculo.id)
+        if (veiculo.id == id) {
+            const novoVeiculo = new veiculos()
+            novoVeiculo.id = veiculo.id
+            novoVeiculo.placa = req.body.placa
+            novoVeiculo.chassi = req.body.chassi
+            novoVeiculo.renavam = req.body.renavam
+            novoVeiculo.modelo = req.body.modelo
+            novoVeiculo.marca = req.body.marca
+            novoVeiculo.ano = req.body.ano
+            control.delete(novoVeiculo)
+            control.write(novoVeiculo)
+            return console.log('Veículo editado')
+        } else {
+            return console.log('Veículo não encontrado')
+        }
+    })
 })
-router.delete('/:id', (req, res) => {
-    res = control.create
+router.delete('/delete/:id', jsonParser, (req, res) => {
+    const dados = control.read()
+    const id = req.params.id
+    dados.forEach(veiculo => {
+        console.log(veiculo.id)
+        if (veiculo.id == id) {
+            control.delete(veiculo)
+            return console.log('Veículo deletado')
+        } else {
+            return console.log('Veículo não encontrado')
+        }
+    })
 })
--- src/data/connection.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-const db = () => {
-    const read = fs.readFileSync('veiculos.json', { encoding: 'utf8' })
-    const write = fs.writeFileSync('veiculos.json', JSON.stringify(veiculos), { encoding: 'utf8' })
-}
-module.exports = db
